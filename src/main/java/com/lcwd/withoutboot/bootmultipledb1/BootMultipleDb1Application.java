63be0db37b66615c2c93593ed706c2735d874955
Kamal Bajaj <bajajkamal076@gmail.com>
2025-01-21 08:12:20 +0530

    Kamal Bajaj

--- src/main/java/com/lcwd/withoutboot/bootmultipledb1/BootMultipleDb1Application.java
+++ src/main/java/com/lcwd/withoutboot/bootmultipledb1/BootMultipleDb1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootMultipleDb1Application {
 
     public static void main(String[] args) {
+        System.out.println("This is me Kamal Bajaj");
         SpringApplication.run(BootMultipleDb1Application.class, args);
     }
 
