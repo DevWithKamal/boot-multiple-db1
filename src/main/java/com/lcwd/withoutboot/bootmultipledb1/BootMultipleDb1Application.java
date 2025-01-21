e0ca96ec8ca69877919c79bb2a372be299de4f44
Kamal Bajaj <bajajkamal076@gmail.com>
2025-01-21 08:10:24 +0530

    init commit

---
+++ src/main/java/com/lcwd/withoutboot/bootmultipledb1/BootMultipleDb1Application.java
@@ -0,0 +1,13 @@
+package com.lcwd.withoutboot.bootmultipledb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMultipleDb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMultipleDb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcwd/withoutboot/bootmultipledb1/BootMultipleDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.withoutboot.bootmultipledb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMultipleDb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
